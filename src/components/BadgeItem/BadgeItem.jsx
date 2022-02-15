f5ef0c9408242b11dc32be2fac986e41cf580edd
aliahshakirin <aliahshakirin@gmail.com>
2022-02-15 00:18:12 -0500

    Add badge data

--- src/components/BadgeItem/BadgeItem.jsx
+++ src/components/BadgeItem/BadgeItem.jsx
@@ -3,10 +3,6 @@ import React from 'react';
 import './badgeitem.css';
 
 export default function BadgeItem({badge, descr}) {
-    const[badges, setBadges] = useState([
-        {id: 1, badgeName: "Design Thinking", descr: "desc1"},
-        {id: 2, badgeName: "Digital Craft", descr: "desc2..."}
-      ]);
 
     return (
       <div className="badge-item">
--- src/pages/Badges/Badges.jsx
+++ src/pages/Badges/Badges.jsx
@@ -3,11 +3,44 @@ import axios from 'axios';
 import {useHistory} from 'react-router-dom';
 
 import './badges.css';
+import PageTitle from '../../components/PageTitle/PageTitle';
 
 export default function Badges() {
+
+    const history = useHistory();
+
+    const[badges, setBadges] = useState([
+        {id: 1, badgeName: "Design Thinking", descr: "desc1"},
+        {id: 2, badgeName: "Digital Craft", descr: "desc2..."},
+        {id: 3, badgeName: "Electronics Foundation", descr: "desc1"},
+        {id: 4, badgeName: "Hand Craft", descr: "desc1"}
+      ]);
+
     return (
-        <div>
-            <h1>Hello</h1>
+        <div className="content">
+            <div className="badges-header">
+                <PageTitle
+                text="Digital Badges"
+                />
+                <button
+                onClick={() => history.push('/')}
+                className="button"
+                >
+                {"<--"}Go Back Home
+                </button>
+            </div>
+
+            <div className="badges-list">
+                {badges && badges.map( (badge, index) =>
+                <div 
+                    className="badge-item"
+                    key={`${badge.id}-${index}`}
+                >
+                    <p>{badge.badgeName}</p>
+                    <p className="badge-descr">{badge.descr}</p>
+                </div>
+                )}
+            </div>
         </div>
     )
 }
