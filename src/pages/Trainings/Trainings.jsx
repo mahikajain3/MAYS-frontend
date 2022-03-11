ab9fac3f699da46b031029bfe7819115787b0872
aliahshakirin <aliahshakirin@gmail.com>
2022-03-11 00:24:43 -0500

    Create trainings page

--- src/pages/Trainings/Trainings.jsx
+++ src/pages/Trainings/Trainings.jsx
@@ -3,7 +3,29 @@ import axios from 'axios';
 import {useHistory} from 'react-router-dom';
 import { backendurl } from '../../config';
 
+import './trainings.css';
+import PageTitle from '../../components/PageTitle/PageTitle';
+
 export default function Trainings() {
-    
+    const[trainings, setTrainings] = useState(undefined);
+
+    const history = useHistory();
+
+    return (
+        <div className="content">
+            <div className="trainings-header">
+                <PageTitle
+                text="Trainings"
+                />
+                <button
+                onClick={() => history.push('/')}
+                className="button"
+                >
+                {"<--"}Go Back Home
+                </button>
+            </div>
+        
+        </div>
+    )
 
 }
