af75b9e7bde5b9e637ece6f0001bd34d46baa38b
YipengWang1007 <yw3548@nyu.edu>
2022-03-11 21:12:53 -0500

    Badge items pictures updated

--- src/components/BadgeItem/BadgeItem.jsx
+++ src/components/BadgeItem/BadgeItem.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import './badgeitem.css';
+import title from "../../title.png";
 
 export default function BadgeItem({name}) {
 
     return (
       <div className="badge-item">
         <p> {name} </p>
-          
+        <></>
       </div>
     );
   }
