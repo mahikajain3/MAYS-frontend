ee64ac43cf4879c0475c28dff4b07bc1b36c6d01
aliahshakirin <aliahshakirin@gmail.com>
2022-03-23 21:40:13 -0400

    Connect create workshop to backend

--- src/pages/Workshops/Workshops.jsx
+++ src/pages/Workshops/Workshops.jsx
@@ -1,18 +1,21 @@
 import axios from 'axios';
 import React, {useEffect, useState} from 'react';
 import {useHistory} from 'react-router-dom';
+import { backendurl } from '../../config';
 
 import './workshops.css';
 import PageTitle from '../../components/PageTitle/PageTitle';
 import WorkshopItem from '../../components/WorkshopItem/WorkshopItem';
-import { backendurl } from '../../config';
+
 
 export default function Workshops() {
 
     const [workshops, setWorkshops] = useState(undefined);
     const [error, setError] = useState(undefined);
+    const [newWorkshopName, setnewWorkshopName] = useState('');
 
     const [refresh, setRefresh] = useState(undefined);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     const history = useHistory();
 
@@ -30,8 +33,34 @@ export default function Workshops() {
             });
     }, [refresh])
 
+    const handleCreateWorkshop = () => {
+        axios.post(`${backendurl}/workshops/create/${newWorkshopName}`)
+            .then( () => {
+                setIsModalOpen(false);
+                setRefresh(refresh + 1);
+            })
+            .catch(error => {
+                setError(error);
+                console.log(error);
+            })
+    }
+
     return (
         <div className="content">
+            {isModalOpen &&
+                <div className="create-modal">
+                    <input
+                    className="workshop-input"
+                    placeholder="Workshop Name"
+                    value={newWorkshopName}
+                    onChange={ (e) => setnewWorkshopName(e.target.value)}
+                    />
+                    <div className="create-actions">
+                        <button className="button" onClick={handleCreateWorkshop}>Create New Workshop</button>
+                        <button className="button" onClick={() => setIsModalOpen(false)}>Cancel</button>
+                    </div>
+                </div>
+            }
             <div className="workshops-header">
                 <PageTitle
                 text="Workshops"
@@ -53,6 +82,10 @@ export default function Workshops() {
                         <p> Sorry there are no workshops right now... Come back later</p>
                     </div>
                 )}
+            </div>
+
+            <div>
+                <button className="page-button" onClick={() => setIsModalOpen(true)}> Add New Workshop </button>
             </div>
         </div>
         
