f6033afc2e5a20168117ae93f52bb9b5b4e4939c
aliahshakirin <aliahshakirin@gmail.com>
2022-03-27 17:21:37 -0400

    Add delete badges

--- src/components/BadgeTable/BadgeTableData.jsx
+++ src/components/BadgeTable/BadgeTableData.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
+import { backendurl } from '../../config';
 
 import './badgetable.css';
 
 export default function BadgeTableData({badge:{badgeName, descr}}) {
+
+    const [error, setError] = useState(undefined);
+
+    function refreshPage() {
+        window.location.reload(false);
+      }
+
+    const handleDeleteBadge = () => {
+        axios.delete(`${backendurl}/badges/delete/${badgeName}`)
+          .then((response) => {
+            console.log(response.data);
+            refreshPage();
+          })
+          .catch(error => {
+            setError(error);
+            console.log(error);
+          })
+      }
+
     return(
         <tr key={badgeName}>
             <td>{badgeName}</td>
@@ -10,7 +31,7 @@ export default function BadgeTableData({badge:{badgeName, descr}}) {
             <td>default training</td>
             <td>default workshop</td>
             <td><button>Edit</button></td>
-            <td><button>Delete</button></td>
+            <td><button className ="button" onClick={handleDeleteBadge}>Delete</button></td>
         </tr>
     )
 
