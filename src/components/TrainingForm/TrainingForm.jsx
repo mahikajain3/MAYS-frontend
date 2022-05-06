42edf747d7911d52e0c5f62c95367f21f8cd8406
aliahshakirin <47250708+aliahshakirin@users.noreply.github.com>
2022-05-06 16:08:50 -0400

    Connect to delete training endpoint

--- src/components/TrainingForm/TrainingForm.jsx
+++ src/components/TrainingForm/TrainingForm.jsx
@@ -8,10 +8,10 @@ export default function TrainingForm({trainingName}) {
 
     const history = useHistory();
 
-
     const [error, setError] = useState(undefined);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [newTrainingName, setnewTrainingName] = useState(undefined);
+
 
     const handleUpdateTraining = () => {
         axios.put(`${backendurl}/trainings/update/${trainingName}/${newTrainingName}`)
@@ -23,6 +23,18 @@ export default function TrainingForm({trainingName}) {
             console.log(error);
           })
       }
+
+    const handleDeleteTraining = () => {
+        axios.delete(`${backendurl}/trainings/delete/${trainingName}`)
+            .then((response) => {
+                console.log(response.data);
+                history.push('/edit-training');
+            })
+            .catch(error => {
+                setError(error);
+                console.log(error);
+            })
+    }
 
     const handleSubmit = (event) => {
         handleUpdateTraining();
@@ -50,8 +62,23 @@ export default function TrainingForm({trainingName}) {
                     onChange={(e) => setnewTrainingName(e.target.value)}
                     />
                 </label>
-                <input type="submit" value="Submit" />
+                <input type="submit" value="Update" />
             </form>
+
+            {isModalOpen && 
+                <div className="create-modal">
+                    <div className="create-actions">
+                        <h1>Delete {trainingName}</h1>
+                        <p>Are you sure you want to delete training?</p>
+                            <button className="button" onClick={handleDeleteTraining}>Delete</button>
+                            <button className="button" onClick={() => setIsModalOpen(false)}> Cancel </button>
+                    </div>
+                </div>
+            }
+
+            <div>
+                <button className="delete-button" onClick={() => setIsModalOpen(true)}> Delete </button>
+            </div>
 
             
         </div>
